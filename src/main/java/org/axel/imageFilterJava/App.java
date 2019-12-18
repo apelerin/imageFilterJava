75cc4bfe10eef6844e5d648c3807cc1f2990f5dc
Axel P <arcanynka@gmail.com>
2019-12-18 13:50:12 +0100

    Add method to find all images names in imgIn and how the filters are called

--- src/main/java/org/axel/imageFilterJava/App.java
+++ src/main/java/org/axel/imageFilterJava/App.java
@@ -1,9 +1,25 @@
 package org.axel.imageFilterJava;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class App {
     public static void main(String[] args) {
-        String pathName = "imgIn/poussePasMemeDansLesOrties.jpg";
+        ArrayList<String> list_path = getPaths();
         grayScaleFilter myGrayScaleFilter = new grayScaleFilter();
-        myGrayScaleFilter.applyFilter(pathName);
+        list_path.forEach(myGrayScaleFilter::applyFilter);
+    }
+
+    public static ArrayList<String> getPaths() {
+        ArrayList<String> list_path = new ArrayList<String>();
+        File folder = new File("imgIn");
+        File[] listOfFiles = folder.listFiles();
+
+        for (int i = 0; i < listOfFiles.length; i++) {
+            if (listOfFiles[i].isFile()) {
+                list_path.add("imgIn/" + listOfFiles[i].getName());
+            }
+        }
+        return list_path;
     }
 }

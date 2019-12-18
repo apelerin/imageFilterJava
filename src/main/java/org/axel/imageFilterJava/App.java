c670c3b81f208e744e64243baff5163798d713e4
Axel P <arcanynka@gmail.com>
2019-12-18 16:17:13 +0100

    Add cli configuration, to be implemented

--- src/main/java/org/axel/imageFilterJava/App.java
+++ src/main/java/org/axel/imageFilterJava/App.java
@@ -1,10 +1,32 @@
 package org.axel.imageFilterJava;
 
+import org.apache.commons.cli.*;
+
 import java.io.File;
 import java.util.ArrayList;
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ParseException {
+        final Options firstOptions = configFirstParameters();
+        final Options options = configParameters(firstOptions);
+
+        final CommandLineParser parser = new DefaultParser();
+        final CommandLine firstLine = parser.parse(firstOptions, args, true);
+        boolean help = firstLine.hasOption("help");
+
+        final CommandLine line = parser.parse(options, args);
+        String input = line.getOptionValue("input");
+        String output = line.getOptionValue("output");
+
+        System.out.println(input + output);
+
+        if (help) {
+            final HelpFormatter formatter = new HelpFormatter();
+            formatter.printHelp("FilterCLI", options, true);
+            System.exit(0);
+        }
+
+
         ArrayList<String> list_path = getRelPath();
         grayScaleFilter myGrayScaleFilter = new grayScaleFilter();
         //pass the names to the fi
@@ -22,5 +44,43 @@ public class App {
             }
         }
         return list_path;
+    }
+
+    public static Options configFirstParameters() {
+        final Option helpOption = Option.builder("h")
+                .longOpt("help")
+                .desc("Display help message")
+                .build();
+
+        final Options firstOptions = new Options();
+        firstOptions.addOption(helpOption);
+        return firstOptions;
+    }
+    public static Options configParameters(final Options firstOptions) {
+
+        final Option inputOption = Option.builder("i")
+                .longOpt("input")
+                .hasArg(true)
+                .argName("input")
+                .required(true)
+                .build();
+
+        final Option outputOption = Option.builder("o")
+                .longOpt("output")
+                .hasArg(true)
+                .argName("output")
+                .required(true)
+                .build();
+
+        final Options options = new Options();
+
+        for (final Option fo : firstOptions.getOptions()) {
+            options.addOption(fo);
+        }
+
+        options.addOption(inputOption);
+        options.addOption(outputOption);
+
+        return options;
     }
 }
